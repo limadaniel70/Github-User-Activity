7c863f0d3089fd5f45ddceb908f7f381624f6e24
Daniel Lima <dnlima77237@gmail.com>
2024-09-14 16:48:28 -0300

    Updated message

--- github-user-activity/parser.py
+++ github-user-activity/parser.py
@@ -18,19 +18,26 @@ def parse(response) -> list[str]:
 def parse_issue(event) -> str:
     repo: str = get_repository(event)
     issue_type: str = event["payload"]["action"]
-    return f"{issue_type} in {repo}"
+    date: str = get_date(event)
+    return f"{issue_type} an issue in {repo} at {date}"
 
 
 def parse_push(event) -> str:
     repo: str = get_repository(event)
     n_of_commits: int = event["payload"]["size"]
-    return f"pushed {n_of_commits} commits into {repo}"
+    date: str = get_date(event)
+    return f"pushed {n_of_commits} commits into {repo} at {date}"
 
 
 def parse_pull_request(event) -> str:
     type: str = event["payload"]["action"]
     repo: str = get_repository(event)
+    date: str = get_date(event)
     return f"{type} a pull request in {repo}"
+
+
+def get_date(event):
+    return event["created_at"]
 
 
 def get_repository(event) -> str:
